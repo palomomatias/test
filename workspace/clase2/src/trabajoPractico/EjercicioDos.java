9d431b1ec6de95d17cbd9bd7214256af47dd543b
palomomatias <36156789+palomomatias@users.noreply.github.com>
2019-08-12 20:45:53 -0300

    Add files via upload

---
+++ workspace/clase2/src/trabajoPractico/EjercicioDos.java
@@ -0,0 +1,17 @@
+package trabajoPractico;
+
+public class EjercicioDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=4;
+		int num2=5;
+		int producto;
+		int suma;
+		producto=num1*num2;
+		suma=num1+num2;
+		System.out.println("La suma es "+suma);
+		System.out.println("El producto es "+producto);
+	}
+
+}
---
+++ workspace/clase2/src/trabajoPractico/EjercicioUno.java
@@ -0,0 +1,15 @@
+package trabajoPractico;
+
+public class EjercicioUno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=0;
+		int multiplicacion;
+		num1=+2;
+		System.out.println(num1);
+		multiplicacion=num1*2;
+		System.out.println(multiplicacion);
+	}
+
+}
